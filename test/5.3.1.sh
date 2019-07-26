39a9c49078506feb2d7c5e56fb52f11314d234ed
Syed Muhmmad Kumail <kumail@Syeds-MacBook-Pro.local>
2019-07-26 15:12:37 +0500

    done till 5.5

--- test/5.3.1.sh
+++ test/5.3.1.sh
@@ -3,6 +3,27 @@
 
 # 5.3.1 - Ensure password creation requirements are configured (Scored)
 
-grep pam_cracklib.so /etc/pam.d/password-auth | grep -E "try_first_pass\s*retry=3\s*minlen=14" || exit $1
+grep pam_pwquality.so /etc/pam.d/password-auth | grep "retry=3" || exit $?
+grep pam_pwquality.so /etc/pam.d/password-auth | grep "try_first_pass" || exit $?
 
-grep pam_cracklib.so /etc/pam.d/system-auth | grep -E "try_first_pass\s*retry=3\s*minlen=14" || exit $1
+min_length=$(grep ^minlen /etc/security/pwquality.conf | awk {'print $3'})
+if [ -z $min_length ]
+then
+echo "exiting 1"
+exit 1
+fi
+
+if [ $min_length -lt 14 ]
+then
+echo "less than 14"
+exit 1
+fi
+
+grep ^dcredit /etc/security/pwquality.conf | grep "dcredit = 1" || exit $?
+
+grep ^lcredit /etc/security/pwquality.conf | grep "lcredit = -1" || exit $?
+
+grep ^ocredit /etc/security/pwquality.conf | grep "ocredit = -1" || exit $?
+
+grep ^ucredit /etc/security/pwquality.conf | grep "ucredit = -1" || exit $?
+
--- test/5.5.sh
+++ test/5.5.sh
@@ -2,6 +2,7 @@
 # ** AUTO GENERATED **
 
 # 5.5 - Ensure root login is restricted to system console (Not Scored)
+# need to manually validate the list of terminals
 
 cat /etc/securetty
 
